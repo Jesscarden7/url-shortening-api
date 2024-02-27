214cdc2f6566ce21f8ad5cb968b6b699bf5d2c12
Jessica Cardeño <cardenojessica07@gmail.com>
2024-02-27 15:19:13 -0500

    GetStarted component mobile design

--- src/components/main/getStarted/GetStarted.tsx
+++ src/components/main/getStarted/GetStarted.tsx
@@ -1,9 +1,15 @@
 import styles from './GetStarted.module.css'
+import working from '../../../assets/images/illustration-working.svg'
 
 function GetStarted() {
   return ( 
     <div className={styles.getStarted}>
-      GetStarted
+      <img src={working} />
+      <div className={styles.callToAction}>
+        <h1>More than just shorter links</h1>
+        <p>Build your brand's recognition and get detailed insights on how your links are performing.</p>
+        <button>Get Started</button>
+      </div>
     </div>
    );
 }
